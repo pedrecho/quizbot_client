831964cd652aeb2fbbc556df16f0dc76dbdec74d
grifondopala <grifondopala@gmail.com>
2022-10-08 23:53:07 +0300

    'main menu v1.1'

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React from 'react';
 import './App.css';
 
 const tg = window.Telegram.WebApp;
-//tg.initDataUnsafe?.user?.first_name
 function App() {
 
   const [name, setName] = React.useState(tg.initDataUnsafe?.user?.first_name)
 
   return (
-      <div>
-          <p>Бобро пожаловать</p>
-          <input defaultValue={name} onChange={e => setName(e.target.value)}/>
-          <button>Присоединиться</button>
-          <button>Создать комнату</button>
+      <div id='mainMenuDiv'>
+          <p id='greetingText'>Бобро пожаловать</p>
+          <input id='nameInput' autoComplete="off" defaultValue={name} onChange={e => setName(e.target.value)}/>
+          <div id='buttonsDiv'>
+              <button className='buttonMainMenu'>Присоединиться</button>
+              <button className='buttonMainMenu'>Создать комнату</button>
+          </div>
       </div>
   );
 
