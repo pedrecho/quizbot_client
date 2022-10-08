3dcd8ae9447da79daa33e2cad94ff7f39d08f62d
grifondopala <grifondopala@gmail.com>
2022-10-08 21:56:12 +0300

    'first commit'

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  const [count, setCount] = React.useState(0)
+
+  const plusFunc = (val) => {
+    setCount(count + val);
+  };
+
+  return (
+      <div className='hello'>
+        <button onClick={() => plusFunc(1)}>+</button>
+        {count}
+        <button onClick={() => plusFunc(-1)}>-</button>
+      </div>
+  );
+}
+
+export default App;
