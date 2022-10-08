46e9577becf941dab12ff649f38db4dbed412e86
grifondopala <grifondopala@gmail.com>
2022-10-08 23:05:12 +0300

    'main menu v1.0'

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './App.css';
 
 const tg = window.Telegram.WebApp;
-
+//tg.initDataUnsafe?.user?.first_name
 function App() {
+
+  const [name, setName] = React.useState(tg.initDataUnsafe?.user?.first_name)
 
   return (
       <div>
-          <p>Мой юзернэйм: {tg.initDataUnsafe?.user?.username}</p>
+          <p>Бобро пожаловать</p>
+          <input defaultValue={name} onChange={e => setName(e.target.value)}/>
+          <button>Присоединиться</button>
+          <button>Создать комнату</button>
       </div>
   );
 
